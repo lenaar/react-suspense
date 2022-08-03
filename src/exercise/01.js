285f7f67d0159c878ee69b02b6f657929f4ad331
Elena Rakhimova <elenara@kth.se>
2022-08-03 17:20:09 +0200

    fix: use error boundary and throw error

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,24 +3,29 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {fetchPokemon, PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
 let pokemon
+let error
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
-let pokemonPromise = fetchPokemon('pikachu').then(p => (pokemon = p))
+let pokemonPromise = fetchPokemon('pikachu')
+  .then(p => (pokemon = p))
+  .catch(e => (error = e))
 
 function PokemonInfo() {
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (error) throw new Error(error.message)
   if (!pokemon) throw pokemonPromise
+
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
   return (
@@ -37,10 +42,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <React.Suspense fallback={<div>Loading Pokemon...</div>}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
