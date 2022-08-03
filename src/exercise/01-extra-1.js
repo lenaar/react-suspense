41740b2a3101f36a9880c74dcddc251613e9c774
Elena Rakhimova <elenara@kth.se>
2022-08-03 17:01:51 +0200

    chore: add initial file before extra exercise 1.1

---
+++ src/exercise/01-extra-1.js
@@ -0,0 +1,49 @@
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
+import {fetchPokemon, PokemonDataView} from '../pokemon'
+
+// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+
+// 🐨 create a variable called "pokemon" (using let)
+let pokemon
+
+// We don't need the app to be mounted to know that we want to fetch the pokemon
+// named "pikachu" so we can go ahead and do that right here.
+// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
+// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
+// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+let pokemonPromise = fetchPokemon('pikachu').then(p => (pokemon = p))
+
+function PokemonInfo() {
+  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
+  // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) throw pokemonPromise
+  // if the code gets it this far, then the pokemon variable is defined and
+  // rendering can continue!
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
+        <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <PokemonInfo />
+        </React.Suspense>
+      </div>
+    </div>
+  )
+}
+
+export default App
